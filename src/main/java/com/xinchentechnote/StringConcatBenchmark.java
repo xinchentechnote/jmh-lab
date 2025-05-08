7923ec65faefd4b2bf0a367864efd97c66732f13
xinchen <283235702@qq.com>
2025-05-08 16:28:43 +0800

    test:add jmh test

--- src/main/java/com/xinchentechnote/StringConcatBenchmark.java
+++ src/main/java/com/xinchentechnote/StringConcatBenchmark.java
@@ -12,27 +12,38 @@ public class StringConcatBenchmark {
     private String str1 = "Hello";
     private String str2 = "World";
     private String str3 = "Java";
+    private int count = 100;
 
     @Benchmark
     public String testStringBuilder() {
         StringBuilder sb = new StringBuilder();
-        sb.append(str1);
-        sb.append(str2);
-        sb.append(str3);
+        for (int i = 0; i < count; i++) {
+            sb.append(str1);
+            sb.append(str2);
+            sb.append(str3);
+        }
         return sb.toString();
     }
 
     @Benchmark
     public String testStringBuffer() {
         StringBuffer sb = new StringBuffer();
-        sb.append(str1);
-        sb.append(str2);
-        sb.append(str3);
+        for (int i = 0; i < count; i++) {
+            sb.append(str1);
+            sb.append(str2);
+            sb.append(str3);
+        }
         return sb.toString();
     }
 
     @Benchmark
     public String testStringPlus() {
-        return str1 + str2 + str3;
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result += str1;
+            result += str2;
+            result += str3;
+        }
+        return result;
     }
 }
