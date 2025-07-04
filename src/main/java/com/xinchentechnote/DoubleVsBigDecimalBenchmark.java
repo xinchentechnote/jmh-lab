849e4e5d76ebf4b16ca780d8710d32c640c7dd41
xinchen <283235702@qq.com>
2025-07-04 10:23:16 +0800

    feat:DoubleVsBigDecimalBenchmark

--- src/main/java/com/xinchentechnote/DoubleVsBigDecimalBenchmark.java
+++ src/main/java/com/xinchentechnote/DoubleVsBigDecimalBenchmark.java
@@ -11,23 +11,77 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 public class DoubleVsBigDecimalBenchmark {
 
-    private double d1 = 123456.789;
-    private double d2 = 98765.4321;
-    private double d3 = 0.5;
-
-    private BigDecimal bd1 = new BigDecimal("123456.789");
-    private BigDecimal bd2 = new BigDecimal("98765.4321");
-    private BigDecimal bd3 = new BigDecimal("0.5");
-
     @Benchmark
     public double doubleCal() {
+        double d1 = 123456.789;
+        double d2 = 98765.4321;
+        double d3 = 0.5;
         return d1 + (d2 - d1 / d2) * d3;
     }
 
     @Benchmark
     public BigDecimal bigDecimalCal() {
         // bd1 + (bd2 - bd1 / bd2) * bd3
+        BigDecimal bd1 = new BigDecimal("123456.789");
+        BigDecimal bd2 = new BigDecimal("98765.4321");
+        BigDecimal bd3 = new BigDecimal("0.5");
+
         return bd1.add((bd2.subtract(bd1.divide(bd2, 8, RoundingMode.HALF_UP))).multiply(bd3));
     }
 
+    @Benchmark
+    public double doubleAdd() {
+        double d1 = 123456.789;
+        double d2 = 98765.4321;
+        return d1 + d2;
+    }
+
+    @Benchmark
+    public BigDecimal bigDecimalAdd() {
+        BigDecimal bd1 = new BigDecimal("123456.789");
+        BigDecimal bd2 = new BigDecimal("98765.4321");
+        return bd1.add(bd2);
+    }
+
+    @Benchmark
+    public double doubleSubtract() {
+        double d1 = 123456.789;
+        double d2 = 98765.4321;
+        return d1 - d2;
+    }
+
+    @Benchmark
+    public BigDecimal bigDecimalSubtract() {
+        BigDecimal bd1 = new BigDecimal("123456.789");
+        BigDecimal bd2 = new BigDecimal("98765.4321");
+        return bd1.subtract(bd2);
+    }
+
+    @Benchmark
+    public double doubleMultiply() {
+        double d1 = 123456.789;
+        double d2 = 98765.4321;
+        return d1 * d2;
+    }
+
+    @Benchmark
+    public BigDecimal bigDecimalMultiply() {
+        BigDecimal bd1 = new BigDecimal("123456.789");
+        BigDecimal bd2 = new BigDecimal("98765.4321");
+        return bd1.multiply(bd2);
+    }
+
+    @Benchmark
+    public double doubleDivide() {
+        double d1 = 123456.789;
+        double d2 = 98765.4321;
+        return d1 / d2;
+    }
+
+    @Benchmark
+    public BigDecimal bigDecimalDivide() {
+        BigDecimal bd1 = new BigDecimal("123456.789");
+        BigDecimal bd2 = new BigDecimal("98765.4321");
+        return bd1.divide(bd2);
+    }
 }
