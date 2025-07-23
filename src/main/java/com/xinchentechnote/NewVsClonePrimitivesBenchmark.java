5eb84b81f54837e9c07f88d77f7f8b78dbbec457
xinchen <283235702@qq.com>
2025-07-23 23:11:36 +0800

    feat:add NewVsClonePrimitivesBenchmark

---
+++ src/main/java/com/xinchentechnote/NewVsClonePrimitivesBenchmark.java
@@ -0,0 +1,62 @@
+package com.xinchentechnote;
+
+import org.openjdk.jmh.annotations.*;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(3)
+public class NewVsClonePrimitivesBenchmark {
+
+    private static class DataHolder implements Cloneable {
+        // All primitive types
+        byte b;
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+        char c;
+        boolean bool;
+
+        public DataHolder() {
+            this.b = (byte) 1;
+            this.s = (short) 2;
+            this.i = 3;
+            this.l = 4L;
+            this.f = 5.0f;
+            this.d = 6.0;
+            this.c = '7';
+            this.bool = true;
+        }
+
+        @Override
+        public DataHolder clone() {
+            try {
+                return (DataHolder) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError();
+            }
+        }
+    }
+
+    private DataHolder prototype;
+
+    @Setup
+    public void setup() {
+        prototype = new DataHolder();
+    }
+
+    @Benchmark
+    public DataHolder newInstance() {
+        return new DataHolder();
+    }
+
+    @Benchmark
+    public DataHolder cloneInstance() {
+        return prototype.clone();
+    }
+}
